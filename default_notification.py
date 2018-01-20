76313272b1beb3cd4fc23e9c85bdc8dc1052e825
Sassraka <34189631+Sassraka@users.noreply.github.com>
2018-01-20 12:42:13 -0500

    Add files via upload

---
+++ default_notification.py
@@ -0,0 +1,7 @@
+import kivy
+kivy.require('1.0.9')
+from kivy.lang import Builder
+from kivy.uix.gridlayout import GridLayout
+from kivy.properties import NumericProperty
+from kivy.app import App
+
